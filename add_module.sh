ba6de6c6214d927ff8fb4af42a0e3683722a3ed4
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-28 18:21:53 -0500

    Update add_module.sh to require repository provider and adjust submodule URL

--- add_module.sh
+++ add_module.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Check if at least one argument is provided (argc >= 2, since argv[0] is script name)
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
     echo "Error: Please provide at least one repository name as an argument"
-    echo "Usage: $0 <repo_name>"
+    echo "Usage: $0 <repo_name> <repo_provider> (e.g., https://github.com/rodrigoo-r/)"
     exit 1
 fi
 
 # Store the first argument
 REPO_NAME="$1"
+REPO_PROVIDER="$2"
 
 # Execute git submodule commands
-git submodule add "https://github.com/rodrigoo-r/${REPO_NAME}.git" "include/fluent/${REPO_NAME}" &&
+git submodule add "${REPO_PROVIDER}/${REPO_NAME}.git" "include/fluent/${REPO_NAME}" &&
 git submodule update --init --recursive &&
 git add .gitmodules include/fluent &&
-git commit -m "Add submodule" &&
+git commit -m "Add submodule ${REPO_NAME}" &&
 git push origin master
 
 # Check if commands were successful
